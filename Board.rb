ac02f2ef3ccb389ca72c90fa36ba50ccfb2cacf1
App Academy Student <student@appacademy.io>
2014-03-13 10:40:14 -0400

    Began Checkers game

---
+++ Board.rb
@@ -0,0 +1,14 @@
+BOARD_SIZE = 8
+
+class Board
+  attr_accessor :grid
+
+  def initialize(fill = true)
+    setup_grid(fill)
+  end
+
+  def perform_jump
+
+  end
+
+end
---
+++ Piece.rb
@@ -0,0 +1,10 @@
+class Piece
+  attr_accessor :king
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+
+
+end
